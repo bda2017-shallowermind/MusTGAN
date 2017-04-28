6ce5d470964b61d1a94f183b14ebe13a7dd51073
Yongjoo Ahn <anyj0527@gmail.com>
2017-04-28 16:43:03 +0900

    add tqdm progress bar and fix some logical error

--- magenta/magenta/models/nsynth/tfrecord_writer.py
+++ magenta/magenta/models/nsynth/tfrecord_writer.py
@@ -10,33 +10,23 @@ import numpy as np
 import librosa
 import tensorflow as tf
 import os, sys
+from tqdm import tqdm
 
 
-# should change this directory path
 dataset_dir = "/home/anyj0527/dataset/wav/16khz/"
-wavfiles_list = sorted([
-    os.path.join(dataset_dir, fname) for fname in tf.gfile.ListDirectory(dataset_dir)
-    if (lambda f: f.lower().endswith(".wav"))
-])
-# should change this directory path
 sliced_dataset_dir = "./dataset/musicnet_4s_sliced/"
-sliced_file_list = sorted([
-    os.path.join(sliced_dataset_dir, fname) for fname 
-        in tf.gfile.ListDirectory(sliced_dataset_dir)
-    if (lambda f: f.lower().endswith(".wav"))
-])
 
 
 def slice_audio(file_list, slice_length):
   """Slice audio files into sliece_length chunks (maybe 64000 samples)"""
-  for fname in file_list:
+  for fname in tqdm(file_list):
     y, sr = librosa.load(fname, sr=None)
-    total_duration_sec = len(y)//slice_length
-    for v in range(0, len(y), slice_length):
-      sliced = y[v:v+slice_length]
-      sliced_file_name = fname.split("/")[-1] + \
-          str(v//slice_length) + "s-" + str(total_duration_sec) + "s.wav"
-      if len(sliced) == slice_length:
+    total_duration_4sec = len(y)//slice_length
+    for v in tqdm(range(0, len(y), slice_length)):
+      if v+slice_length < len(y):
+        sliced = y[v:v+slice_length]
+        sliced_file_name = fname.split("/")[-1].split(".")[0] + "--" + \
+            str(v//slice_length).zfill(3) + "-" + str(total_duration_4sec) + ".wav"
         librosa.output.write_wav(sliced_dataset_dir + sliced_file_name, sliced, sr)
 
 
@@ -44,7 +34,7 @@ def tfrecord_write(sliced_file_list):
   """Make tfrecord consists of sliced audio files"""
   # should change this output tfrecord path
   writer = tf.python_io.TFRecordWriter("musicnet.tfrecord")
-  for fname in sliced_file_list:
+  for fname in tqdm(sliced_file_list):
     y, sr = librosa.load(fname, sr=None)
     features = tf.train.Features(
         feature = { 'audio': tf.train.Feature(float_list=tf.train.FloatList(value=y)), } )
@@ -56,8 +46,22 @@ def tfrecord_write(sliced_file_list):
 
 if __name__ == "__main__":
   """Usage example
+
+  # should change this directory path
+  wavfiles_list = sorted([
+      os.path.join(dataset_dir, fname) for fname in tf.gfile.ListDirectory(dataset_dir)
+      if (lambda f: f.lower().endswith(".wav"))
+  ])
   slice_audio(wavfiles_list, 64000)
+
+  # should change this directory path
+  sliced_file_list = sorted([
+      os.path.join(sliced_dataset_dir, fname) for fname 
+          in tf.gfile.ListDirectory(sliced_dataset_dir)
+      if (lambda f: f.lower().endswith(".wav"))
+  ])
   tfrecord_write(sliced_file_list)
+
   """
   print("Hello, Jason!")
 
