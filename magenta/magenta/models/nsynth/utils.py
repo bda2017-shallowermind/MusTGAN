360303fb8a870b691c5d3a362226d8012cf1c94d
Yongjoo Ahn <anyj0527@gmail.com>
2017-06-01 00:16:41 +0900

    fix load_wav part

--- magenta/magenta/models/nsynth/utils.py
+++ magenta/magenta/models/nsynth/utils.py
@@ -55,10 +55,8 @@ def load_wav(path):
   Returns:
     The 16bit data in the range [-1, 1].
   """
-  _, data_16bit = scipy.io.wavfile.read(tf.gfile.Open(path, "r"))
-  # Assert we are working with 16-bit audio.
-  assert data_16bit.dtype == np.int16
-  return data_16bit.astype(np.float32) / 2**15
+  _, data_16bit = scipy.io.wavfile.read(path)
+  return data_16bit.astype(np.float32)
 
 
 def mu_law(x, mu=255):
