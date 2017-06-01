d86b29374bd312bc1eca6bf76d030fdb99f23141
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2017-06-01 14:27:40 +0900

    basic model class structure

---
+++ magenta/magenta/models/nsynth/gan/model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+class MusTGAN(object):
+  def __init__(self, batch_size, mode='train'):
+    self.learning_rate_schedule = {
+        0: 3e-4,
+        2500: 1e-4,
+        5000: 6e-5,
+        10000: 4e-5,
+        20000: 2e-5,
+        40000: 1e-5,
+        60000: 6e-6,
+        80000: 2e-6,
+    }
+    self.num_stages = 10
+    self.filter_length = 3
+    self.ae_num_stages = 10
+    self.ae_num_layers = 30
+    self.ae_filter_length = 3
+    self.ae_width = 128
+    self.ae_bottleneck_width = 16
+    self.batch_size = batch_size
+    self.tv_const = 1e-6
+
+  def f():
+    pass
+
+  def pretrain_model():
+    pass
+
+  def g():
+    pass
+
+  def d():
+    pass
