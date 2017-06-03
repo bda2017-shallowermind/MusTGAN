51afede04bb596b7f5116db2ffb0ed2123dfda78
Gyeongin Yu <gyeonginyu@gmail.com>
2017-06-03 15:56:08 +0900

    fix

--- magenta/magenta/models/nsynth/gan/solver.py
+++ magenta/magenta/models/nsynth/gan/solver.py
@@ -113,21 +113,21 @@ class Solver(object):
         saver = tf.train.Saver()
         tf.logging.info("Start running")
 
+        start_time = time.time()
         for step in xrange(self.model.pretrain_iter):
           if step > 0 and step % self.log_period == 0:
             duration = time.time() - start_time
+            start_time = time.time()
             _, l, acc = sess.run([
                 model["train_op"],
                 #model["summary_op"],
                 model["loss"],
                 model["accuracy"]])
             # summary_writer.add_summary(summary, step)
-            tf.logging.info("%s step: %d/%d, loss: %.6f, acc: %.2f, step/sec: %.2f"
-                % (datetime.now(), step, self.model.pretrain_iter, l, acc, self.log_period / duration))
-            start_time = time.time()
+            tf.logging.info("step: %d, loss: %.6f, acc: %.4f, step/sec: %.3f"
+                % (step, l, acc, self.log_period / duration))
           else:
             sess.run(model["train_op"])
-            start_time = time.time()
 
           if step % self.ckpt_period == 0:
             saver.save(sess, os.path.join(
