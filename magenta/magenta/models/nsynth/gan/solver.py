b63ec53760af61a9eb7c7d7c0cb885f8f283eac9
Yongjoo <anyj0527@gmail.com>
2017-06-03 19:41:26 +0900

    implement solver train

--- magenta/magenta/models/nsynth/gan/solver.py
+++ magenta/magenta/models/nsynth/gan/solver.py
@@ -115,9 +115,8 @@ class Solver(object):
 
         start_time = time.time()
         for step in xrange(self.model.pretrain_iter):
-          if step > 0 and step % self.log_period == 0:
+          if step > 0 and (step + 1) % self.log_period == 0:
             duration = time.time() - start_time
-            start_time = time.time()
             _, l, acc = sess.run([
                 model["train_op"],
                 #model["summary_op"],
@@ -126,6 +125,7 @@ class Solver(object):
             # summary_writer.add_summary(summary, step)
             tf.logging.info("step: %d, loss: %.6f, acc: %.4f, step/sec: %.3f"
                 % (step, l, acc, self.log_period / duration))
+            start_time = time.time()
           else:
             sess.run(model["train_op"])
 
@@ -134,7 +134,88 @@ class Solver(object):
                 self.pretrain_path, 'model.ckpt'), global_step=step)
 
   def train(self):
-    return
+    num_gpus = self.model.num_gpus
+
+    with tf.Graph().as_default() as graph:
+      src_train_files = glob.glob(self.src_wav_path + "/*")
+      trg_train_files = glob.glob(self.trg_wav_path + "/*")
+      assert len(src_train_files)==len(trg_train_files)
+      if (len(src_train_files) < num_gpus):
+        raise RuntimeError("Number of training files: %d, " \
+            "while number of gpus: %d" % (len(src_train_files), num_gpus))
+      elif (len(src_train_files) > num_gpus):
+        tf.logging.warning("Number of training files: %d, " \
+            "while number of gpus: %d" % (len(src_train_files), num_gpus))
+      src_train_files = src_train_files[:num_gpus]
+      trg_train_files = trg_train_files[:num_gpus]
+
+      src_wavs = []
+      trg_wavs = []
+      for i in range(num_gpus):
+        with tf.device('/gpu:%d' % i):
+          with tf.name_scope('gpu_name_scope_%d' % i):
+            src_wav, _ = self.get_batch_from_queue(
+                src_train_files[i], self.model.batch_size)
+            trg_wav, _ = self.get_batch_from_queue(
+                trg_train_files[i], self.model.batch_size)
+            src_wavs.append(src_wav)
+            trg_wavs.append(trg_wav)
+
+      model = self.model.build_train_model(src_wavs, trg_wavs)
+
+      with tf.Session(config=self.sess_config) as sess:
+        # TODO: load pretrained f
+        if self.from_scratch == False:
+          variables_to_restore = slim.get_model_variables(scpoe='f')
+          pass
+        # TODO: load trained whole model
+        else:
+          pass
+
+        global_init = tf.global_variables_initializer()
+        sess.run(global_init)
+        tf.logging.info("Finished initialization")
+
+        tf.train.start_queue_runners(sess=sess)
+        summary_writer = tf.summary.FileWriter(
+            logdir=self.train_path,
+            graph=sess.graph)
+        tf.train.write_graph(sess.graph, self.train_path,
+            "graph.pbtxt", as_text=True)
+        saver = tf.train.Saver()
+        tf.logging.info("Start training")
+
+        start_time = time.time()
+        f_train_period = self.model.f_train_period
+        d_train_iter_per_step = self.model.d_train_iter_per_step
+        g_train_iter_per_step = self.model.g_train_iter_per_step
+        for step in xrange(self.model.train_iter):
+          # train d and g
+          for _ in xrange(d_train_iter_per_step):
+            sess.run(model["d_train_op"])
+
+          for _ in xrange(g_train_iter_per_step):
+            sess.run(model["g_train_op"])
+
+          # train f periodically
+          if step % f_train_period == 0:
+            sess.run(model["f_train_op"])
+
+          # logging loss info
+          if step > 0 and (step + 1) % self.log_period == 0:
+            duration = time.time() - start_time
+            dl, gl, fl = sess.run([
+                model["d_loss"],
+                model["g_loss"],
+                model["f_loss"])
+            tf.logging.info("step: %d, d_loss: %.6f, " \
+                "g_loss: %.6f, f_loss: %.6f, step/sec: %.3f"
+                % (step, dl, gl, fl, self.log_period / duration))
+            start_time = time.time()
+
+          if step % self.ckpt_period == 0:
+            saver.save(sess, os.path.join(
+                self.train_path, 'model.ckpt'), global_step=step)
 
   def eval(self):
     return
